c96dedcbf78b66779c462a60aed12017dd937f3a
wroberts4 <wroberts4@wisc.edu>
2022-08-12 15:00:30 -0500

    Make a more general test case

--- test.py
+++ test.py
@@ -8,24 +8,36 @@ import time
 import os
 
 
-def test_func(x):
-    print(x)
+def sleep_one(x, y):
+    print(x, y)
     time.sleep(1)
-    return x
+    return x, y
+
+
+def sleep_two(x, y):
+    print(x, y)
+    time.sleep(2)
+    return x, y
+
 
 if __name__ == '__main__':
     client = Client(os.environ["SOCKET"])
     num_workers = len(client.scheduler_info()['workers'])
-    input = range(4 * num_workers)
+    xs = range(2 * num_workers)
+    ys = range(2 * num_workers)
 
     start = time.time()
     with multiprocessing.Pool(num_workers) as p:
-        output = p.map(test_func, input)
-    print(output)
+        output1 = p.starmap(sleep_one, zip(xs, ys))
+        output2 = p.starmap(sleep_two, zip(xs, ys))
+    print((output1, output2))
     print('multiprocessing:', time.time() - start)
 
     start = time.time()
-    dask_input = db.from_sequence(input, npartitions=2 * num_workers)
-    output = dask_input.map(test_func)
-    print(da.compute(output))
+    npartitions = 2 * num_workers
+    dask_xs = db.from_sequence(xs, npartitions=npartitions)
+    dask_ys = db.from_sequence(ys, npartitions=npartitions)
+    output1 = dask.bag.map(sleep_one, dask_xs, dask_ys)
+    output2 = dask.bag.map(sleep_two, dask_xs, dask_ys)
+    print(da.compute(output1, output2))
     print('dask:', time.time() - start)
